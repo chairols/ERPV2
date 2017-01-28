8858347e9957427fc50b348462842c0f13a767b0
chair <chair@DESKTOP-GS34M4R>
2017-01-28 20:09:13 -0300

    - Se cambió el layout del menu

--- ERPV2/application/views/layout_alela/menu.php
+++ ERPV2/application/views/layout_alela/menu.php
@@ -50,15 +50,8 @@
                     <span class=" fa fa-angle-down"></span>
                   </a>
                   <ul class="dropdown-menu dropdown-usermenu pull-right">
-                    <li><a href="javascript:;"> Profile</a></li>
-                    <li>
-                      <a href="javascript:;">
-                        <span class="badge bg-red pull-right">50%</span>
-                        <span>Settings</span>
-                      </a>
-                    </li>
-                    <li><a href="javascript:;">Help</a></li>
-                    <li><a href="/usuarios/logout/"><i class="fa fa-sign-out pull-right"></i> Log Out</a></li>
+                      <li><a href="/usuarios/perfil/"><i class="fa fa-user pull-right"></i> Perfil</a></li>
+                      <li><a href="/usuarios/logout/"><i class="fa fa-sign-out pull-right"></i> Salir</a></li>
                   </ul>
                 </li>
 
