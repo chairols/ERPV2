e288a0672c435221dd5929939fda9e4a2e19689e
chair <chair@DESKTOP-GS34M4R>
2017-01-29 17:18:07 -0300

    - Se cambió el layout de modificar almacen

--- ERPV2/application/controllers/almacenes.php
+++ ERPV2/application/controllers/almacenes.php
@@ -128,10 +128,10 @@ class Almacenes extends CI_Controller {
         
         $data['almacen'] = $this->almacenes_model->get_where(array('idalmacen' => $idalmacen));
         
-        $this->load->view('layout/header', $data);
-        $this->load->view('layout/menu');
+        $this->load->view('layout_alela/header', $data);
+        $this->load->view('layout_alela/menu');
         $this->load->view('almacenes/modificar');
-        $this->load->view('layout/footer');
+        $this->load->view('layout_alela/footer');
     }
     
     public function ver($idalmacen = null) {
