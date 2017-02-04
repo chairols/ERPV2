beff91debefa5c81aeac41ba741ded5dc2626061
chair <chair@DESKTOP-GS34M4R>
2017-02-04 17:40:25 -0300

    - Se actualizó la vista de listar roles

--- ERPV2/application/controllers/roles.php
+++ ERPV2/application/controllers/roles.php
@@ -28,10 +28,10 @@ class Roles extends CI_Controller {
         
         $data['roles'] = $this->roles_model->gets();
         
-        $this->load->view('layout_alela/header', $data);
-        $this->load->view('layout_alela/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('roles/index');
-        $this->load->view('layout_alela/footer');
+        $this->load->view('layout_lte/footer');
     }
     
     public function agregar() {
--- ERPV2/application/views/roles/index.php
+++ ERPV2/application/views/roles/index.php
@@ -1,61 +1,56 @@
-<div class="right_col" role="main">
-    <div class="page-title">
-        <div class="title_left">
-            <h3><?=$title?></h3>
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1><?=$title?></h1>
+    </section>
+    
+    <section class="content">
+        <div class="row-fluid">
+            <ul class="nav nav-tabs nav-tabs-justified">
+                <li class="active"><a href="/roles/">Listar Roles</a></li>
+                <li><a href="/roles/agregar/">Agregar Rol</a></li>
+                <li><a href="/roles/menu/">Roles-Menú</a></li>
+            </ul>
         </div>
-    </div>
-    
-    <div class="clearfix"></div>
-    
-    <div class="row">
-        <ul class="nav nav-tabs bar_tabs nav-tabs-justified">
-            <li class="active"><a href="/roles/">Listar Roles</a></li>
-            <li><a href="/roles/agregar/">Agregar Rol</a></li>
-            <li><a href="/roles/menu/">Roles-Menú</a></li>
-        </ul>
-    </div>
-    
-    <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Listar Roles</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-                        <li><a class="close-link"><i class="fa fa-close"></i></a></li>
-                    </ul>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-                    <table id="datatable-responsive" class="table table-condensed table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th><strong>Rol</strong></th>
-                                <th><strong>Acción</strong></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($roles as $rol) { ?>
-                            <tr>
-                                <td><?=$rol['rol']?></td>
-                                <td>
-                                    <a href="/roles/menu/<?=$rol['idrol']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
-                                        <button class="btn btn-xs btn-warning">
-                                            <i class="fa fa-edit"></i>
-                                        </button>
-                                    </a>
-                                    <a href="/log/ver/roles/<?=$rol['idrol']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
-                                        <button class="btn btn-xs btn-info">
-                                            <i class="fa fa-clock-o"></i>
-                                        </button>
-                                    </a>
-                                </td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+        
+        <br>
+        
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?=$title?></h3>
+                    </div>
+                    <div class="box-body">
+                        <table id="datatable" class="table table-condensed table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                    <th><strong>Rol</strong></th>
+                                    <th><strong>Acción</strong></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($roles as $rol) { ?>
+                                <tr>
+                                    <td><?=$rol['rol']?></td>
+                                    <td>
+                                        <a href="/roles/menu/<?=$rol['idrol']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                            <button class="btn btn-xs btn-warning">
+                                                <i class="fa fa-edit"></i>
+                                            </button>
+                                        </a>
+                                        <a href="/log/ver/roles/<?=$rol['idrol']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
+                                            <button class="btn btn-xs btn-info">
+                                                <i class="fa fa-clock-o"></i>
+                                            </button>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
