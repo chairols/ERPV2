adcfd3ef85d49269efc415ae1c5e7aa7e0488416
chair <chair@DESKTOP-GS34M4R>
2017-02-04 17:47:07 -0300

    - Se actualizó la vista de agregar rol

--- ERPV2/application/controllers/roles.php
+++ ERPV2/application/controllers/roles.php
@@ -77,10 +77,10 @@ class Roles extends CI_Controller {
             }
         }
         
-        $this->load->view('layout_alela/header', $data);
-        $this->load->view('layout_alela/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('roles/agregar');
-        $this->load->view('layout_alela/footer');
+        $this->load->view('layout_lte/footer');
     }
     
     public function menu($idrol = null) {
