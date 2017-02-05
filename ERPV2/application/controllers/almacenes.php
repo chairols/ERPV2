0748507898a698802c9e28cbe761c8a4f58ad400
chair <chair@DESKTOP-GS34M4R>
2017-02-05 17:21:37 -0300

    - Se actualizó la vista de agregar almacén

--- ERPV2/application/controllers/almacenes.php
+++ ERPV2/application/controllers/almacenes.php
@@ -76,10 +76,10 @@ class Almacenes extends CI_Controller {
             }
         }
         
-        $this->load->view('layout_alela/header', $data);
-        $this->load->view('layout_alela/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('almacenes/agregar');
-        $this->load->view('layout_alela/footer');
+        $this->load->view('layout_lte/footer');
     }
     
     public function modificar($idalmacen = null) {
