cd22f6321d915d3a7a17388ceb7d2d8a0008d10c
chair <chair@DESKTOP-GS34M4R>
2017-02-06 01:46:20 -0300

    - Se actualizó la vista de listar numeros de serie

--- ERPV2/application/controllers/numeroserie.php
+++ ERPV2/application/controllers/numeroserie.php
@@ -30,10 +30,10 @@ class Numeroserie extends CI_Controller {
         
         $data['numeroserie'] = $this->numeros_serie_model->gets();
         
-        $this->load->view('layout_alela/header', $data);
-        $this->load->view('layout_alela/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('numeroserie/index');
-        $this->load->view('layout_alela/footer');
+        $this->load->view('layout_lte/footer');
     }
     
     public function trazabilidad($numeroserie) {
--- ERPV2/application/views/numeroserie/index.php
+++ ERPV2/application/views/numeroserie/index.php
@@ -1,55 +1,48 @@
-<div class="right_col" role="main">
-    <div class="page-title">
-        <div class="title_left">
-            <h3><?=$title?></h3>
-        </div>
-    </div>
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1><?=$title?></h1>
+    </section>
     
-    <div class="clearfix"></div>
-    
-    <div class="row">
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2>Listar Números de Serie</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
-                        <li><a class="close-link"><i class="fa fa-close"></i></a></li>
-                    </ul>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-                    <table id="datatable-responsive-desc" class="table table-striped table-bordered table-condensed">
-                        <thead>
-                            <tr>
-                                <th><strong>Número de Serie</strong></th>
-                                <th><strong>Fábrica</strong></th>
-                                <th><strong>Orden de Trabajo</strong></th>
-                                <th><strong>Artículo</strong></th>
-                                <th><strong>Acción</strong></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach($numeroserie as $ns) { ?>
-                            <tr>
-                                <td><?=$ns['numero_serie']?></td>
-                                <td><?=$ns['fabrica']?></td>
-                                <td><a href="/ots/ver/<?=$ns['idot']?>/"><?=$ns['numero_ot']?></a></td>
-                                <td><?=$ns['producto']?> <?=$ns['articulo']?></td>
-                                <td>
-                                    <a href="/numeroserie/trazabilidad/<?=$ns['numero_serie']?>/">
-                                        <button class="btn btn-xs btn-success" data-pacement="top" data-toggle="tooltip" data-original-title="Trazabilidad">
-                                            <i class="fa fa-exchange"></i>
-                                        </button>
-                                    </a>
-                                    <a href="/numeroserie/trazabilidad/<?=$ns['numero_serie']?>/"><i class="icon-exchange alert-success tooltips" data-pacement="top" data-toggle="tooltip" data-original-title="Trazabilidad"></i></a>
-                                </td>
-                            </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
+    <section class="content">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?=$title?></h3>
+                    </div>
+                    <div class="box-body">
+                        <table id="datatable-desc" class="table table-striped table-bordered table-condensed">
+                            <thead>
+                                <tr>
+                                    <th><strong>Número de Serie</strong></th>
+                                    <th><strong>Fábrica</strong></th>
+                                    <th><strong>Orden de Trabajo</strong></th>
+                                    <th><strong>Artículo</strong></th>
+                                    <th><strong>Acción</strong></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach($numeroserie as $ns) { ?>
+                                <tr>
+                                    <td><?=$ns['numero_serie']?></td>
+                                    <td><?=$ns['fabrica']?></td>
+                                    <td><a href="/ots/ver/<?=$ns['idot']?>/"><?=$ns['numero_ot']?></a></td>
+                                    <td><?=$ns['producto']?> <?=$ns['articulo']?></td>
+                                    <td>
+                                        <a href="/numeroserie/trazabilidad/<?=$ns['numero_serie']?>/">
+                                            <button class="btn btn-xs btn-success" data-pacement="top" data-toggle="tooltip" data-original-title="Trazabilidad">
+                                                <i class="fa fa-exchange"></i>
+                                            </button>
+                                        </a>
+                                        <a href="/numeroserie/trazabilidad/<?=$ns['numero_serie']?>/"><i class="icon-exchange alert-success tooltips" data-pacement="top" data-toggle="tooltip" data-original-title="Trazabilidad"></i></a>
+                                    </td>
+                                </tr>
+                                <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
