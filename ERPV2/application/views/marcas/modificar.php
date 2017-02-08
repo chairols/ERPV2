7ca7e3cb04bbc3b03217337fec7e3eb4b99c9ca1
chair <chair@DESKTOP-GS34M4R>
2017-02-07 23:10:46 -0300

    - Se actualizo la vista de modificar marca

--- ERPV2/application/views/marcas/modificar.php
+++ ERPV2/application/views/marcas/modificar.php
@@ -1,13 +1,9 @@
-<div id="main-content">
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <div class="span12">
-                <h3 class="page-title">
-                    <?=$title?>
-                </h3>
-            </div>
-        </div>
-        
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1><?=$title?></h1>
+    </section>
+    
+    <section class="content">
         <div class="row-fluid">
             <ul class="nav nav-tabs nav-tabs-justified">
                 <li><a href="/marcas/">Listar Marcas</a></li>
@@ -18,37 +14,33 @@
             </ul>
         </div>
         
-        <div class="row-fluid">
-            <div class="span6">
-                <div class="widget blue">
-                    <div class="widget-title">
-                        <h4><i class="icon-reorder"></i> Agregar Marca</h4>
-                        <span class="tools">
-                            <a href="javascript:;" class="icon-chevron-down"></a>
-                            <a href="javascript:;" class="icon-remove"></a>
-                        </span>
+        <br>
+        
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="widget-body">
+                    <div class="box-body">
                         <form method="POST" class="form-horizontal">
-                            <div class="control-group">
-                                <label class="control-label">Marca</label>
-                                <div class="controls">
-                                    <input type="text" maxlength="100" class="span12" name="marca" value="<?=$marca['marca']?>" autofocus>
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Marca</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <input type="text" maxlength="100" class="form-control" name="marca" value="<?=$marca['marca']?>" autofocus>
                                 </div>
                             </div>
-                            <div class="form-actions">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="icon-save"></i> Guardar
-                                </button>
-                                <button type="reset" class="btn btn-danger">
-                                    <i class="icon-remove"></i> Limpiar
-                                </button>
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                    <button type="submit" class="btn btn-success">Modificar</button>
+                                    <button type="reset" class="btn btn-primary">Limpiar</button>
+                                </div>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </div>
-
