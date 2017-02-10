ac71f7e5438fe85accee674ffdc9b4bd7c20e8aa
chair <chair@DESKTOP-GS34M4R>
2017-02-10 16:36:34 -0300

    - Se actualizó la vista de listar productos

--- ERPV2/application/views/productos/index.php
+++ ERPV2/application/views/productos/index.php
@@ -1,32 +1,34 @@
-<div id="main-content">
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <div class="span12">
-                <h3 class="page-title">
-                    <?=$title?>
-                </h3>
-            </div>
-        </div>
-        
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1><?=$title?></h1>
+    </section>
+    
+    <section class="content">
         <div class="row-fluid">
             <ul class="nav nav-tabs nav-tabs-justified">
                 <li class="active"><a href="/productos/">Listar Productos</a></li>
                 <li><a href="/productos/agregar/">Agregar Producto</a></li>
                 <li><a href="/productos/modificar/">Modificar Producto</a></li>
             </ul>
-            
-            <div class="row-fluid">
-                <div class="span12">
-                    <div class="widget blue">
-                        <div class="widget-title">
-                            <h4><i class="icon-reorder"></i> Productos</h4>
-                            <span class="tools">
-                                <a href="javascript:;" class="icon-chevron-down"></a>
-                                <a href="javascript:;" class="icon-remove"></a>
-                            </span>
+        </div>
+        
+        <br>
+        
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?=$title?></h3>
+                    </div>
+                    <div id="gears">
+                        <div class="text-center">
+                            <img src="/assets/AdminLTE-2.3.11/gears.gif">
+                            <br><br>
                         </div>
-                        <div class="widget-body">
-                            <table class="table table-condensed table-hover table-bordered" id="sample_1">
+                    </div>
+                    <div id="tabla" style="display: none;">
+                        <div class="box-body">
+                            <table class="table table-condensed table-hover table-bordered" id="datatable">
                                 <thead>
                                     <tr>
                                         <th><strong>Producto</strong></th>
@@ -38,15 +40,26 @@
                                     <tr>
                                         <td><?=$producto['producto']?></td>
                                         <td>
-                                            <a href="#">
-                                                <i class="alert-success icon-eye-open tooltips" data-pacement="top" data-toggle="tooltip" data-original-title="Ver"></i>
+                                            <a href="#" data-pacement="top" data-toggle="tooltip" data-original-title="Ver">
+                                                <button class="btn btn-success btn-xs">
+                                                    <i class="fa fa-eye"></i>
+                                                </button>
                                             </a>
-                                            <a href="/productos/modificar/<?=$producto['idproducto']?>"> 
-                                                <i class="alert-info icon-edit tooltips" data-pacement="top" data-toggle="tooltip" data-original-title="Editar"></i>
+                                            <a href="/productos/modificar/<?=$producto['idproducto']?>" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar"> 
+                                                <button class="btn btn-warning btn-xs">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
                                             </a> 
-                                            <a href="#" class="label label-important"><i class="icon-remove"></i></a>
-                                            <a href="/log/ver/productos/<?=$producto['idproducto']?>/">
-                                                <i class="alert-info icon-time tooltips" data-pacement="top" data-toggle="tooltip" data-original-title="Log"></i></a>
+                                            <a href="#" data-pacement="top" data-toggle="tooltip" data-original-title="Eliminar">
+                                                <button class="btn btn-danger btn-xs">
+                                                    <i class="fa fa-trash"></i>
+                                                </button>
+                                            </a>
+                                            <a href="/log/ver/productos/<?=$producto['idproducto']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log">
+                                                <button class="btn btn-info btn-xs">
+                                                    <i class="fa fa-clock-o"></i>
+                                                </button>
+                                            </a>
                                         </td>
                                     </tr>
                                     <?php } ?>
@@ -57,5 +70,13 @@
                 </div>
             </div>
         </div>
-    </div>
+            
+    </section>
 </div>
+
+<script type="text/javascript">
+    function inicio() {
+        $("#gears").hide();
+        $("#tabla").fadeIn(1000);
+    }
+</script>
