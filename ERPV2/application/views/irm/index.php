d2908ac9d99ffa51cd0020ce178ab238eb1a9387
chair <chair@DESKTOP-GS34M4R>
2017-02-11 18:17:44 -0300

    - Se actualizó la vista de listar informes de recepción de materiales

--- ERPV2/application/views/irm/index.php
+++ ERPV2/application/views/irm/index.php
@@ -20,48 +20,63 @@
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="box-body">
-                        <table class="table table-hover table-bordered table-condensed" id="datatable-desc">
-                            <thead>
-                                <tr>
-                                    <th>IRM</th>
-                                    <th>Cantidad</th>
-                                    <th>Artículo</th>
-                                    <th>Proveedor</th>
-                                    <th>Recepcionado</th>
-                                    <th>Fecha</th>
-                                    <th>Controles</th>
-                                    <th>Acción</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($irms as $irm) { ?>
-                                <tr>
-                                    <td><?=$irm['idirm']?></td>
-                                    <td><?=$irm['cantidad']?></td>
-                                    <td><?=$irm['producto']?> <?=$irm['articulo']?></td>
-                                    <td><?=$irm['proveedor']?></td>
-                                    <td><?=$irm['nombre']?> <?=$irm['apellido']?></td>
-                                    <td><?=date('d/m/Y', strtotime($irm['timestamp']))?></td>
-                                    <td>
-                                        <?php foreach($irm['controles'] as $control) { ?>
-                                        <span class="badge bg-green"><?=$control['control']?></span>
-                                        <?php } ?>
-                                    </td>
-                                    <td>
-                                        <a href="/irm/agregar_items/<?=$irm['idirm']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
-                                            <button class="btn btn-warning btn-xs">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
+                    <div id="gears">
+                        <div class="text-center">
+                            <img src="/assets/AdminLTE-2.3.11/gears.gif">
+                            <br><br>
+                        </div>
+                    </div>
+                    <div id="tabla" style="display: none;">
+                        <div class="box-body">
+                            <table class="table table-hover table-bordered table-condensed" id="datatable-desc">
+                                <thead>
+                                    <tr>
+                                        <th>IRM</th>
+                                        <th>Cantidad</th>
+                                        <th>Artículo</th>
+                                        <th>Proveedor</th>
+                                        <th>Recepcionado</th>
+                                        <th>Fecha</th>
+                                        <th>Controles</th>
+                                        <th>Acción</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($irms as $irm) { ?>
+                                    <tr>
+                                        <td><?=$irm['idirm']?></td>
+                                        <td><?=$irm['cantidad']?></td>
+                                        <td><?=$irm['producto']?> <?=$irm['articulo']?></td>
+                                        <td><?=$irm['proveedor']?></td>
+                                        <td><?=$irm['nombre']?> <?=$irm['apellido']?></td>
+                                        <td><?=date('d/m/Y', strtotime($irm['timestamp']))?></td>
+                                        <td>
+                                            <?php foreach($irm['controles'] as $control) { ?>
+                                            <span class="badge bg-green"><?=$control['control']?></span>
+                                            <?php } ?>
+                                        </td>
+                                        <td>
+                                            <a href="/irm/agregar_items/<?=$irm['idirm']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                                <button class="btn btn-warning btn-xs">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </div>
+
+<script type="text/javascript">
+    function inicio() {
+        $("#gears").hide();
+        $("#tabla").fadeIn(1000);
+    }
+</script>
