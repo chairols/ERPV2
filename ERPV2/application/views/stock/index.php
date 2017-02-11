6826508f24888107ff54ebe3a131201033edf82e
chair <chair@DESKTOP-GS34M4R>
2017-02-11 12:17:15 -0300

    - Se le agregó loading a listar stock

--- ERPV2/application/views/stock/index.php
+++ ERPV2/application/views/stock/index.php
@@ -23,45 +23,60 @@
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="box-body">
-                        <table id="datatable" class="table table-striped table-bordered table-condensed">
-                            <thead>
-                                <tr>
-                                    <th><strong>Artículo</strong></th>
-                                    <th><strong>Marca</strong></th>
-                                    <th><strong>Cantidad</strong></th>
-                                    <th><strong>Unidad de Medida</strong></th>
-                                    <th><strong>Acción</strong></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($stock as $s) { ?>
-                                <?php if($s['cantidad'] > 0) { ?>
-                                <tr>
-                                    <td><?=$s['producto']?> <?=$s['articulo']?></td>
-                                    <td><?=$s['marca']?></td>
-                                    <td><?=$s['cantidad']?></td>
-                                    <td><?=$s['medida_larga']?></td>
-                                    <td>
-                                        <a href="/stock/almacenes/<?=$s['idstock']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
-                                            <button class="btn btn-xs btn-warning">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                        </a>
-                                        <a href="/log/ver/stock/<?=$s['idstock']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
-                                            <button class="btn btn-xs btn-info">
-                                                <i class="fa fa-clock-o"></i>
-                                            </button>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php } ?>
-                                <?php } ?>
-                            </tbody>
-                        </table>
+                    <div id="gears">
+                        <div class="text-center">
+                            <img src="/assets/AdminLTE-2.3.11/gears.gif">
+                            <br><br>
+                        </div>
+                    </div>
+                    <div id="tabla" style="display: none;">
+                        <div class="box-body">
+                            <table id="datatable" class="table table-striped table-bordered table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th><strong>Artículo</strong></th>
+                                        <th><strong>Marca</strong></th>
+                                        <th><strong>Cantidad</strong></th>
+                                        <th><strong>Unidad de Medida</strong></th>
+                                        <th><strong>Acción</strong></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($stock as $s) { ?>
+                                    <?php if($s['cantidad'] > 0) { ?>
+                                    <tr>
+                                        <td><?=$s['producto']?> <?=$s['articulo']?></td>
+                                        <td><?=$s['marca']?></td>
+                                        <td><?=$s['cantidad']?></td>
+                                        <td><?=$s['medida_larga']?></td>
+                                        <td>
+                                            <a href="/stock/almacenes/<?=$s['idstock']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                                <button class="btn btn-xs btn-warning">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                            </a>
+                                            <a href="/log/ver/stock/<?=$s['idstock']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
+                                                <button class="btn btn-xs btn-info">
+                                                    <i class="fa fa-clock-o"></i>
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </div>
+
+<script type="text/javascript">
+    function inicio() {
+        $("#gears").hide();
+        $("#tabla").fadeIn(1000);
+    }
+</script>
