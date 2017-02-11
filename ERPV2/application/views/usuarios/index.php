6f921d87a533daccfb12d9416d925b9fc0fde5d5
chair <chair@DESKTOP-GS34M4R>
2017-02-11 12:12:57 -0300

    - Se le agregó loading a listar usuarios

--- ERPV2/application/views/usuarios/index.php
+++ ERPV2/application/views/usuarios/index.php
@@ -20,43 +20,58 @@
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="box-body">
-                        <table id="datatable" class="table table-condensed table-hover table-bordered">
-                            <thead>
-                                <tr>
-                                    <th><strong>Usuario</strong></th>
-                                    <th><strong>Nombre</strong></th>
-                                    <th><strong>Correo</strong></th>
-                                    <th><strong>Rol</strong></th>
-                                    <th><strong>Acción</strong></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($usuarios as $usuario) { ?>
-                                <tr>
-                                    <td><?=$usuario['usuario']?></td>
-                                    <td><?=$usuario['nombre']?> <?=$usuario['apellido']?></td>
-                                    <td><?=$usuario['correo']?></td>
-                                    <td><?=$usuario['rol']?></td>
-                                    <td>
-                                        <a href="/usuarios/modificar/<?=$usuario['idusuario']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
-                                            <button class="btn btn-xs btn-warning">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                        </a>
-                                        <a href="/log/ver/usuarios/<?=$usuario['idusuario']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
-                                            <button class="btn btn-xs btn-info">
-                                                <i class="fa fa-clock-o"></i>
-                                            </button>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
+                    <div id="gears">
+                        <div class="text-center">
+                            <img src="/assets/AdminLTE-2.3.11/gears.gif">
+                            <br><br>
+                        </div>
+                    </div>
+                    <div id="tabla" style="display: none;">
+                        <div class="box-body">
+                            <table id="datatable" class="table table-condensed table-hover table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th><strong>Usuario</strong></th>
+                                        <th><strong>Nombre</strong></th>
+                                        <th><strong>Correo</strong></th>
+                                        <th><strong>Rol</strong></th>
+                                        <th><strong>Acción</strong></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($usuarios as $usuario) { ?>
+                                    <tr>
+                                        <td><?=$usuario['usuario']?></td>
+                                        <td><?=$usuario['nombre']?> <?=$usuario['apellido']?></td>
+                                        <td><?=$usuario['correo']?></td>
+                                        <td><?=$usuario['rol']?></td>
+                                        <td>
+                                            <a href="/usuarios/modificar/<?=$usuario['idusuario']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                                <button class="btn btn-xs btn-warning">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                            </a>
+                                            <a href="/log/ver/usuarios/<?=$usuario['idusuario']?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
+                                                <button class="btn btn-xs btn-info">
+                                                    <i class="fa fa-clock-o"></i>
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </div>
+
+<script type="text/javascript">
+    function inicio() {
+        $("#gears").hide();
+        $("#tabla").fadeIn(1000);
+    }
+</script>
