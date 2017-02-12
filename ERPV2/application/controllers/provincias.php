610f8bf7533483c7209307789b5f7c10d8053ee8
chair <chair@DESKTOP-GS34M4R>
2017-02-12 18:27:30 -0300

    - Se actualizó la vista de Agregar Provincia

--- ERPV2/application/controllers/provincias.php
+++ ERPV2/application/controllers/provincias.php
@@ -77,10 +77,10 @@ class Provincias extends CI_Controller {
             }
         }
         
-        $this->load->view('layout/header', $data);
-        $this->load->view('layout/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('provincias/agregar');
-        $this->load->view('layout/footer');
+        $this->load->view('layout_lte/footer');
     }
     
     public function modificar($idprovincia = null) {
