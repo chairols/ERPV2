7029b4aa54a79223f9acde3d46f24f11b8526137
chair <chair@DESKTOP-GS34M4R>
2017-02-12 18:53:54 -0300

    - Se actualizó la vista de Agregar Contrato

--- ERPV2/application/controllers/contratos.php
+++ ERPV2/application/controllers/contratos.php
@@ -49,76 +49,68 @@ class Contratos extends CI_Controller {
         if($this->form_validation->run() == FALSE) {
             
         } else {
-            /*$datos = array(
+            $datos = array(
                 'idcliente' => $this->input->post('cliente'),
-                'numero_contrato' => $this->input->post('numero_contrato')
+                'numero_contrato' => $this->input->post('numero_contrato'),
+                'descripcion' => $this->input->post('descripcion')
             );
-            $resultado = $this->contratos_model->get_where($datos);
-            
-            if(count($resultado) == 0) { */  // Se anula porque se necesita poder duplicar contratos
-                $datos = array(
-                    'idcliente' => $this->input->post('cliente'),
-                    'numero_contrato' => $this->input->post('numero_contrato'),
-                    'descripcion' => $this->input->post('descripcion')
-                );
-                if($this->input->post('vigencia_desde') == '') {
-                    $datos['vigencia_desde'] = NULL;
-                } else {
-                    $datos['vigencia_desde'] = $this->input->post('vigencia_desde');
-                }
-                if($this->input->post('vigencia_hasta') == '') {
-                    $datos['vigencia_hasta'] = NULL;
-                } else {
-                    $datos['vigencia_hasta'] = $this->input->post('vigencia_hasta');
-                }
-                
-                $config['upload_path'] = "./upload/contratos/";
-                $config['allowed_types'] = '*';
-                $config['encrypt_name'] = true;
-                $config['remove_spaces'] = true;
+            if($this->input->post('vigencia_desde') == '') {
+                $datos['vigencia_desde'] = NULL;
+            } else {
+                $datos['vigencia_desde'] = $this->input->post('vigencia_desde');
+            }
+            if($this->input->post('vigencia_hasta') == '') {
+                $datos['vigencia_hasta'] = NULL;
+            } else {
+                $datos['vigencia_hasta'] = $this->input->post('vigencia_hasta');
+            }
 
-                $this->load->library('upload', $config);
-                $adjunto = null;
+            $config['upload_path'] = "./upload/contratos/";
+            $config['allowed_types'] = '*';
+            $config['encrypt_name'] = true;
+            $config['remove_spaces'] = true;
 
-                if(!$this->upload->do_upload('adjunto')) {
-                    $error = array('error' => $this->upload->display_errors());
-                } else {
-                    $adjunto = array('upload_data' => $this->upload->data());
-                }
+            $this->load->library('upload', $config);
+            $adjunto = null;
 
-                if($adjunto != null) {
-                    $datos['adjunto'] = '/upload/contratos/'.$adjunto['upload_data']['file_name'];
-                } 
+            if(!$this->upload->do_upload('adjunto')) {
+                $error = array('error' => $this->upload->display_errors());
+            } else {
+                $adjunto = array('upload_data' => $this->upload->data());
+            }
 
-                $idcontrato = $this->contratos_model->set($datos);
-                $cliente = $this->clientes_model->get_where(array('idcliente' => $this->input->post('cliente')));
-                
-                $log = array(
-                    'tabla' => 'contratos',
-                    'idtabla' => $idcontrato,
-                    'texto' => 'Se agregó: <br>'
-                     . 'cliente: '.$cliente['cliente'].'<br>'
-                     . 'contrato: '.$this->input->post('numero_contrato').'<br>'
-                     . 'descripción: '.$this->input->post('descripcion').'<br>'
-                     . 'vigencia desde: '.$this->input->post('vigencia_desde').'<br>'
-                     . 'vigencia hasta: '.$this->input->post('vigencia_hasta').'<br>'
-                     . 'adjunto: /upload/contratos/'.$adjunto['upload_data']['file_name'],
-                    'tipo' => 'add',
-                    'idusuario' => $session['SID']
-                );
+            if($adjunto != null) {
+                $datos['adjunto'] = '/upload/contratos/'.$adjunto['upload_data']['file_name'];
+            } 
 
-                $this->log_model->set($log);
-                
-                redirect('/contratos/', 'refresh');
-            /* } */
+            $idcontrato = $this->contratos_model->set($datos);
+            $cliente = $this->clientes_model->get_where(array('idcliente' => $this->input->post('cliente')));
+
+            $log = array(
+                'tabla' => 'contratos',
+                'idtabla' => $idcontrato,
+                'texto' => 'Se agregó: <br>'
+                 . 'cliente: '.$cliente['cliente'].'<br>'
+                 . 'contrato: '.$this->input->post('numero_contrato').'<br>'
+                 . 'descripción: '.$this->input->post('descripcion').'<br>'
+                 . 'vigencia desde: '.$this->input->post('vigencia_desde').'<br>'
+                 . 'vigencia hasta: '.$this->input->post('vigencia_hasta').'<br>'
+                 . 'adjunto: /upload/contratos/'.$adjunto['upload_data']['file_name'],
+                'tipo' => 'add',
+                'idusuario' => $session['SID']
+            );
+
+            $this->log_model->set($log);
+
+            redirect('/contratos/', 'refresh');
         }
         
         $data['clientes'] = $this->clientes_model->gets();
         
-        $this->load->view('layout/header_form', $data);
-        $this->load->view('layout/menu');
+        $this->load->view('layout_lte/header', $data);
+        $this->load->view('layout_lte/menu');
         $this->load->view('contratos/agregar');
-        $this->load->view('layout/footer_form');
+        $this->load->view('layout_lte/footer');
     }
 }
 ?>
--- ERPV2/application/views/contratos/agregar.php
+++ ERPV2/application/views/contratos/agregar.php
@@ -1,13 +1,9 @@
-<div id="main-content">
-    <div class="container-fluid">
-        <div class="row-fluid">
-            <div class="span12">
-                <h3 class="page-title">
-                    <?=$title?>
-                </h3>
-            </div>
-        </div>
-        
+<div class="content-wrapper">
+    <section class="content-header">
+      <h1><?=$title?></h1>
+    </section>
+    
+    <section class="content">
         <div class="row-fluid">
             <ul class="nav nav-tabs nav-tabs-justified">
                 <li><a href="/contratos/">Listar Contratos</a></li>
@@ -18,74 +14,89 @@
             </ul>
         </div>
         
-        <div class="row-fluid">
-            <div class="span6">
-                <div class="widget blue">
-                    <div class="widget-title">
-                        <h4><i class="icon-reorder"></i> Agregar Contrato</h4>
-                        <span class="tools">
-                            <a href="javascript:;" class="icon-chevron-down"></a>
-                            <a href="javascript:;" class="icon-remove"></a>
-                        </span>
+        <br>
+        
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="widget-body">
+                    <div class="box-body">
                         <form method="POST" class="form-horizontal" enctype="multipart/form-data">
-                            <div class="control-group">
-                                <label class="control-label">Cliente</label>
-                                <div class="controls">
-                                    <select name="cliente" class="select2 span12">
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Cliente</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <select name="cliente" class="select2 form-control">
                                         <?php foreach($clientes as $cliente) { ?>
                                         <option value="<?=$cliente['idcliente']?>"><?=$cliente['cliente']?></option>
                                         <?php } ?>
                                     </select>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Número de Contrato</label>
-                                <div class="controls">
-                                    <input type="text" maxlength="100" class="span12" name="numero_contrato" required>
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Número de Contrato</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <input type="text" maxlength="100" class="form-control" name="numero_contrato" required>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Descripción</label>
-                                <div class="controls">
-                                    <input type="text" maxlength="100" class="span12" name="descripcion" required>
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Descripción</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <input type="text" maxlength="100" class="form-control" name="descripcion" required>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Vigencia Desde</label>
-                                <div class="controls">
-                                    <input type="text" class="form-control input-large" id="dp1" name="vigencia_desde" readonly>
-                                    <a onclick="limpiar_campo('dp1');" href="#" class="label label-important"><i class="icon-time"></i> Borrar fecha</a>
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Vigencia Desde</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <div class="input-group date">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                        <input type="text" class="form-control datepicker" id="dp1" name="vigencia_desde" readonly>
+                                        <div class="input-group-addon">
+                                            <a onclick="limpiar_campo('dp1');" href="#" class="label label-danger">
+                                                <i class="fa fa-close"></i>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Vigencia Hasta</label>
-                                <div class="controls">
-                                    <input type="text" class="form-control input-large" id="dp2" name="vigencia_hasta" readonly>
-                                    <a onclick="limpiar_campo('dp2')" href="#" class="label label-important"><i class="icon-time"></i> Borrar fecha</a>
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Vigencia Hasta</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <div class="input-group date">
+                                        <div class="input-group-addon">
+                                            <i class="fa fa-calendar"></i>
+                                        </div>
+                                        <input type="text" class="form-control datepicker" id="dp2" name="vigencia_hasta" readonly>
+                                        <div class="input-group-addon">
+                                            <a onclick="limpiar_campo('dp2');" href="#" class="label label-danger">
+                                                <i class="fa fa-close"></i>
+                                            </a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="control-group">
-                                <label class="control-label">Adjunto</label>
-                                <div class="controls">
-                                    <input type="file" class="span12" name="adjunto">
+                            <div class="form-group">
+                                <label class="control-label col-md-3 col-sm-3 col-sx-12">Adjunto</label>
+                                <div class="col-md-6 col-sm-6 col-sx-12">
+                                    <input type="file" class="form-control" name="adjunto">
                                 </div>
                             </div>
-                            <div class="form-actions">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="icon-save"></i> Guardar
-                                </button>
-                                <button type="reset" class="btn btn-danger">
-                                    <i class="icon-remove"></i> Limpiar
-                                </button>
+                            <div class="ln_solid"></div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                                    <button type="submit" class="btn btn-success">Agregar</button>
+                                    <button type="reset" class="btn btn-primary">Limpiar</button>
+                                </div>
                             </div>
                         </form>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
+        </div> 
+    </section>
 </div>
 
 <script type="text/javascript">
