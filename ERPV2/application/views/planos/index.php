b8d97d05334934c2a7d1cd8c027b92d50b18b1d6
chair <chair@DESKTOP-GS34M4R>
2017-06-05 11:35:12 -0300

    - Se actualizó el loading en listar articulos

--- ERPV2/application/views/planos/index.php
+++ ERPV2/application/views/planos/index.php
@@ -18,16 +18,16 @@
         
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="box">
+                <div class="box box-primary">
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="box-body">
-                        <div id="gears">
-                            <div class="overlay">
-                                <i class="fa fa-refresh fa-spin"></i>
-                            </div>
+                    <div id="gears">
+                        <div class="overlay">
+                            <i class="fa fa-refresh fa-spin"></i>
                         </div>
+                    </div>
+                    <div class="box-body">
                         <div id="tabla" style="display: none">
                             <table id="datatable" class="table table-striped table-bordered table-condensed table-hover">
                                 <thead>
