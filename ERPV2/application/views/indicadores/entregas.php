c68067fc6098dd6ccd9b6fd4d1418825c877dde4
chair <chair@DESKTOP-GS34M4R>
2017-06-06 14:34:19 -0300

    - Se actualizó el loading en indicadores -> entregas

--- ERPV2/application/views/indicadores/entregas.php
+++ ERPV2/application/views/indicadores/entregas.php
@@ -6,7 +6,7 @@
     <section class="content">
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="box">
+                <div class="box box-primary">
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
@@ -62,9 +62,9 @@
                             </div>
                         </form>
                         <div id="gears">
-                            <div class="text-center">
-                                <img src="/assets/AdminLTE-2.3.11/gears.gif">
-                                <br><br>
+                            <br><br><br><br><br><br><br><br><br><br><br><br>
+                            <div class="overlay">
+                                <i class="fa fa-refresh fa-spin"></i>
                             </div>
                         </div>
                         <div id="tabla" style="display: none;">
