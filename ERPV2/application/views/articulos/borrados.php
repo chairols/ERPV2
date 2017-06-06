e9481b6b74861f6d9a753ba32309e64e7c479de4
chair <chair@DESKTOP-GS34M4R>
2017-06-06 14:15:29 -0300

    - Se actualizó el loading en listar artículos borrados

--- ERPV2/application/views/articulos/borrados.php
+++ ERPV2/application/views/articulos/borrados.php
@@ -18,59 +18,73 @@
         
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="box">
+                <div class="box box-primary">
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
-                    <div class="box-body">
-                        <table id="datatable" class="table table-striped table-bordered table-condensed">
-                            <thead>
-                                <tr>
-                                    <th><strong>Artículo</strong></th>
-                                    <th><strong>Producto</strong></th>
-                                    <th><strong>Plano</strong></th>
-                                    <th><strong>Revisión</strong></th>
-                                    <th><strong>Posición</strong></th>
-                                    <th><strong>Acción</strong></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach($articulos as $articulo) { ?>
-                                <tr>
-                                    <td><?=$articulo->getArticulo()?></td>
-                                    <td><?=$articulo->getProducto()->getProducto()?></td>
-                                    <td><?=($articulo->getPlano()->getUrlDelPlano()!='')?"<a href='".$articulo->getPlano()->getUrlDelPlano()."' target='_blank'>".$articulo->getPlano()->getPlano()."</a>":""?></td>
-                                    <td><?=$articulo->getPlano()->getRevision()?></td>
-                                    <td><?=$articulo->getPosicion()?></td>
-                                    <td>
-                                        <a href="/articulos/ver/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver" class="tooltips">
-                                            <button class="btn btn-xs btn-success">
-                                                <i class="fa fa-eye"></i>
-                                            </button>
-                                        </a>
-                                        <a href="/articulos/modificar/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
-                                            <button class="btn btn-xs btn-warning">
-                                                <i class="fa fa-edit"></i>
-                                            </button>
-                                        </a> 
-                                        <a href="/articulos/borrar/<?=$articulo->getId()?>" data-pacement="top" data-toggle="tooltip" data-original-title="Borrar" class="tooltips">
-                                            <button class="btn btn-xs btn-danger">
-                                                <i class="fa fa-trash-o"></i>
-                                            </button>
-                                        </a>
-                                        <a href="/log/ver/articulos/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
-                                            <button class="btn btn-xs btn-info">
-                                                <i class="fa fa-clock-o"></i>
-                                            </button>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php } ?>
-                            </tbody>
-                        </table>
+                    <div id="gears">
+                        <div class="overlay">
+                            <i class="fa fa-refresh fa-spin"></i>
+                        </div>
+                    </div>
+                    <div id="tabla" style="display: none;">
+                        <div class="box-body">
+                            <table id="datatable" class="table table-striped table-bordered table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th><strong>Artículo</strong></th>
+                                        <th><strong>Producto</strong></th>
+                                        <th><strong>Plano</strong></th>
+                                        <th><strong>Revisión</strong></th>
+                                        <th><strong>Posición</strong></th>
+                                        <th><strong>Acción</strong></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach($articulos as $articulo) { ?>
+                                    <tr>
+                                        <td><?=$articulo->getArticulo()?></td>
+                                        <td><?=$articulo->getProducto()->getProducto()?></td>
+                                        <td><?=($articulo->getPlano()->getUrlDelPlano()!='')?"<a href='".$articulo->getPlano()->getUrlDelPlano()."' target='_blank'>".$articulo->getPlano()->getPlano()."</a>":""?></td>
+                                        <td><?=$articulo->getPlano()->getRevision()?></td>
+                                        <td><?=$articulo->getPosicion()?></td>
+                                        <td>
+                                            <a href="/articulos/ver/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver" class="tooltips">
+                                                <button class="btn btn-xs btn-success">
+                                                    <i class="fa fa-eye"></i>
+                                                </button>
+                                            </a>
+                                            <a href="/articulos/modificar/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                                <button class="btn btn-xs btn-warning">
+                                                    <i class="fa fa-edit"></i>
+                                                </button>
+                                            </a> 
+                                            <a href="/articulos/borrar/<?=$articulo->getId()?>" data-pacement="top" data-toggle="tooltip" data-original-title="Borrar" class="tooltips">
+                                                <button class="btn btn-xs btn-danger">
+                                                    <i class="fa fa-trash-o"></i>
+                                                </button>
+                                            </a>
+                                            <a href="/log/ver/articulos/<?=$articulo->getId()?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Ver Log" class="tooltips">
+                                                <button class="btn btn-xs btn-info">
+                                                    <i class="fa fa-clock-o"></i>
+                                                </button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
 </div>
+
+<script type="text/javascript">
+    function inicio() {
+        $("#gears").hide();
+        $("#tabla").fadeIn(1000);
+    }
+</script>
