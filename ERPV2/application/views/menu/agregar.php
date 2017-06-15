b7d7f3962063a7343874191eb6ad88fba7544459
chair <chair@DESKTOP-GS34M4R>
2017-06-15 14:25:18 -0300

    - Se arregló la vista de agregar y modificar menú

--- ERPV2/application/views/menu/agregar.php
+++ ERPV2/application/views/menu/agregar.php
@@ -16,7 +16,7 @@
         
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="box">
+                <div class="box box-primary">
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
