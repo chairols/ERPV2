4f32a5c01dd354ee075f89e4da5ed50ff2f234d2
chair <chair@DESKTOP-GS34M4R>
2017-07-16 21:47:30 -0300

    - Se cambió el select2 por chosen en agregar IRM - Se corrigieron errores de vista

--- ERPV2/application/views/irm/agregar.php
+++ ERPV2/application/views/irm/agregar.php
@@ -16,7 +16,7 @@
         
         <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
-                <div class="box">
+                <div class="box box-primary">
                     <div class="box-header">
                         <h3 class="box-title"><?=$title?></h3>
                     </div>
@@ -25,7 +25,7 @@
                             <div class="form-group">
                                 <label class="control-label col-md-3 col-sm-3 col-sx-12">Proveedor</label>
                                 <div class="col-md-6 col-sm-6 col-sx-12">
-                                    <select name="proveedor" class="form-control select2">
+                                    <select name="proveedor" class="form-control chosen">
                                         <?php foreach($proveedores as $proveedor) { ?>
                                         <option value="<?=$proveedor['idproveedor']?>"><?=$proveedor['proveedor']?></option>
                                         <?php } ?>
@@ -44,39 +44,4 @@
             </div>
         </div>
     </section>
-</div>
-
-<div id="main-content">
-    <div class="container-fluid">
-        
-        <div class="row-fluid">
-            <div class="span6">
-                <div class="widget blue">
-                    
-                    <div class="widget-body">
-                        <form method="POST" class="form-horizontal">
-                            <div class="control-group">
-                                <label class="control-label">Proveedor</label>
-                                <div class="controls">
-                                    <select name="proveedor" class="span12 select2">
-                                        <?php foreach($proveedores as $proveedor) { ?>
-                                        <option value="<?=$proveedor['idproveedor']?>"><?=$proveedor['proveedor']?></option>
-                                        <?php } ?>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="form-actions">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="icon-save"></i> Guardar
-                                </button>
-                                <button type="reset" class="btn btn-danger">
-                                    <i class="icon-remove"></i> Limpiar
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 </div>
